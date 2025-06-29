e933c207d1e1916a3e9aa3d456a2aa52049c2e6a
neilhanda83 <neilhanda2@gmail.com>
2025-06-29 12:48:53 +0000

    [Gemini Generated] [SCC] Remediation for finding: 95d468ba7c634ad9ac4003f78926ff2e, Project: 643618479314, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,107 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# Local variable for the target project ID from IAM_BINDINGS
+locals {
+  target_project_id = "pam-dp-maf-1724303557-y1-2"
+}
+
+# IAM Binding updates for project: pam-dp-maf-1724303557-y1-2
+
+# REMOVE: roles/iam.serviceAccountTokenCreator for serviceAccount:643618479314-compute@developer.gserviceaccount.com
+resource "google_project_iam_member_remove" "remove_sa_643618479314_token_creator" {
+  project = local.target_project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:643618479314-compute@developer.gserviceaccount.com"
+}
+
+# ADD: roles/iam.serviceAccountOpenIdTokenCreator for serviceAccount:643618479314-compute@developer.gserviceaccount.com
+resource "google_project_iam_member" "add_sa_643618479314_openid_token_creator" {
+  project = local.target_project_id
+  role    = "roles/iam.serviceAccountOpenIdTokenCreator"
+  member  = "serviceAccount:643618479314-compute@developer.gserviceaccount.com"
+}
+
+# REMOVE: roles/owner for serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com
+resource "google_project_iam_member_remove" "remove_sa_environmentgate_owner" {
+  project = local.target_project_id
+  role    = "roles/owner"
+  member  = "serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com"
+}
+
+# ADD: roles/cloudfunctions.admin for serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com
+resource "google_project_iam_member" "add_sa_environmentgate_cloudfunctions_admin" {
+  project = local.target_project_id
+  role    = "roles/cloudfunctions.admin"
+  member  = "serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com"
+}
+
+# ADD: roles/iam.serviceAccountUser for serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com
+resource "google_project_iam_member" "add_sa_environmentgate_service_account_user" {
+  project = local.target_project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com"
+}
+
+# ADD: roles/resourcemanager.projectIamAdmin for serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com
+resource "google_project_iam_member" "add_sa_environmentgate_project_iam_admin" {
+  project = local.target_project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com"
+}
+
+# ADD: roles/vpcaccess.viewer for serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com
+resource "google_project_iam_member" "add_sa_environmentgate_vpcaccess_viewer" {
+  project = local.target_project_id
+  role    = "roles/vpcaccess.viewer"
+  member  = "serviceAccount:environmentgate-admin@pam-dp-maf.iam.gserviceaccount.com"
+}
+
+# REMOVE: roles/owner for serviceAccount:643618479314-compute@developer.gserviceaccount.com
+resource "google_project_iam_member_remove" "remove_sa_643618479314_owner" {
+  project = local.target_project_id
+  role    = "roles/owner"
+  member  = "serviceAccount:643618479314-compute@developer.gserviceaccount.com"
+}
+
+# ADD: roles/run.viewer for serviceAccount:643618479314-compute@developer.gserviceaccount.com
+resource "google_project_iam_member" "add_sa_643618479314_run_viewer" {
+  project = local.target_project_id
+  role    = "roles/run.viewer"
+  member  = "serviceAccount:643618479314-compute@developer.gserviceaccount.com"
+}
+
+# REMOVE: roles/owner for serviceAccount:maf-test@pam-dp-maf-1724303557-y1-2.iam.gserviceaccount.com
+resource "google_project_iam_member_remove" "remove_sa_maf_test_owner" {
+  project = local.target_project_id
+  role    = "roles/owner"
+  member  = "serviceAccount:maf-test@pam-dp-maf-1724303557-y1-2.iam.gserviceaccount.com"
+}
+
+# ADD: roles/run.servicesInvoker for serviceAccount:maf-test@pam-dp-maf-1724303557-y1-2.iam.gserviceaccount.com
+resource "google_project_iam_member" "add_sa_maf_test_run_services_invoker" {
+  project = local.target_project_id
+  role    = "roles/run.servicesInvoker"
+  member  = "serviceAccount:maf-test@pam-dp-maf-1724303557-y1-2.iam.gserviceaccount.com"
+}
+
+# REMOVE: roles/resourcemanager.projectIamAdmin for serviceAccount:47826083103-compute@developer.gserviceaccount.com
+resource "google_project_iam_member_remove" "remove_sa_47826083103_project_iam_admin" {
+  project = local.target_project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "serviceAccount:47826083103-compute@developer.gserviceaccount.com"
+}
+
+# ADD: organizations/9454078371/roles/CustomRole262 for serviceAccount:47826083103-compute@developer.gserviceaccount.com
+resource "google_project_iam_member" "add_sa_47826083103_custom_role_262" {
+  project = local.target_project_id
+  role    = "organizations/9454078371/roles/CustomRole262"
+  member  = "serviceAccount:47826083103-compute@developer.gserviceaccount.com"
+}
+
+# REMOVE: roles/editor for serviceAccount:643618479314-compute@developer.gserviceaccount.com
+resource "google_project_iam_member_remove" "remove_sa_643618479314_editor" {
+  project = local.target_project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:643618479314-compute@developer.gserviceaccount.com"
+}
