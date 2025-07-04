b13a35afd07360ab823af50f9c19d4080710a606
neilhanda83 <neilhanda2@gmail.com>
2025-07-04 06:38:05 +0000

    [Gemini Generated] [SCC] Remediation for finding: 58a69ee321844b4e9542e28373bd84ba, Project: 447257701837, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,42 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM Binding updates for group:landingzone-eng@google.com
+
+# REMOVE roles/bigquery.dataEditor
+resource "google_project_iam_member_remove" "remove_bigquery_dataeditor_for_landingzone_eng" {
+  project = data.google_project.current.project_id
+  role    = "roles/bigquery.dataEditor"
+  member  = "group:landingzone-eng@google.com"
+}
+
+# REMOVE roles/bigquery.user
+resource "google_project_iam_member_remove" "remove_bigquery_user_for_landingzone_eng" {
+  project = data.google_project.current.project_id
+  role    = "roles/bigquery.user"
+  member  = "group:landingzone-eng@google.com"
+}
+
+# ADD roles/bigquery.dataViewer
+resource "google_project_iam_member" "add_bigquery_dataviewer_for_landingzone_eng" {
+  project = data.google_project.current.project_id
+  role    = "roles/bigquery.dataViewer"
+  member  = "group:landingzone-eng@google.com"
+  lifecycle {
+    ignore_changes = [
+      condition,
+    ]
+  }
+}
+
+# ADD roles/bigquery.jobUser
+resource "google_project_iam_member" "add_bigquery_jobuser_for_landingzone_eng" {
+  project = data.google_project.current.project_id
+  role    = "roles/bigquery.jobUser"
+  member  = "group:landingzone-eng@google.com"
+  lifecycle {
+    ignore_changes = [
+      condition,
+    ]
+  }
+}
