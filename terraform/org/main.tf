06b51ec8841cf4e4009741e8a78dc7714610bad1
neilhanda83 <neilhanda2@gmail.com>
2025-07-13 02:22:15 +0000

    [Gemini Generated] [SCC] Remediation for finding: 1cffb96390dd42e4a04820680c50fbb6, Project: 76480087717, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,15 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM binding for project shishir-aiposturegen-test
+resource "google_project_iam_member_remove" "remove_org_admin_krgaurv" {
+  project = "shishir-aiposturegen-test"
+  role    = "roles/resourcemanager.organizationAdmin"
+  member  = "user:krgaurv@google.com"
+}
+
+resource "google_project_iam_member" "add_custom_role_krgaurv" {
+  project = "shishir-aiposturegen-test"
+  role    = "organizations/9454078371/roles/CustomRole262"
+  member  = "user:krgaurv@google.com"
+}
