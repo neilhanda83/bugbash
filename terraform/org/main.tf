0dc3bfb65aaba3088dc9dc6c2026696070a22a85
neilhanda83 <neilhanda2@gmail.com>
2025-07-27 02:25:19 +0000

    [Gemini Generated] [SCC] Remediation for finding: 1e2db6da3cdf40c495242e7d0ac2f571, Project: 388769014795, Category UNUSED_IAM_ROLE

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,16 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# Remove IAM binding for roles/servicemanagement.admin for user:adeepanshu@google.com
+resource "google_project_iam_member_remove" "remove_servicemanagement_admin_adeepanshu" {
+  project = "gcloud-testing-project-346315"
+  role    = "roles/servicemanagement.admin"
+  member  = "user:adeepanshu@google.com"
+}
+
+# Remove IAM binding for roles/serviceusage.serviceUsageConsumer for user:adeepanshu@google.com
+resource "google_project_iam_member_remove" "remove_serviceusage_consumer_adeepanshu" {
+  project = "gcloud-testing-project-346315"
+  role    = "roles/serviceusage.serviceUsageConsumer"
+  member  = "user:adeepanshu@google.com"
+}
