5b03891d00ff1d63eb6e51c261f352ca387ce6da
neilhanda83 <neilhanda2@gmail.com>
2025-08-29 01:51:04 +0000

    [Gemini Generated] [SCC] Remediation for finding: 2d0d472add664612aff1b3b9ea49770a, Project: 119897990899, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,32 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM Bindings for project sdw-non-conf-3be8be-8231 and member group:slz-blr@google.com
+
+# REMOVE roles/bigquery.user
+resource "google_project_iam_member_remove" "remove_bigquery_user_slz_blr" {
+  project = "sdw-non-conf-3be8be-8231"
+  role    = "roles/bigquery.user"
+  member  = "group:slz-blr@google.com"
+}
+
+# ADD roles/bigquery.jobUser
+resource "google_project_iam_member" "add_bigquery_job_user_slz_blr" {
+  project = "sdw-non-conf-3be8be-8231"
+  role    = "roles/bigquery.jobUser"
+  member  = "group:slz-blr@google.com"
+}
+
+# REMOVE roles/bigquery.dataEditor
+resource "google_project_iam_member_remove" "remove_bigquery_data_editor_slz_blr" {
+  project = "sdw-non-conf-3be8be-8231"
+  role    = "roles/bigquery.dataEditor"
+  member  = "group:slz-blr@google.com"
+}
+
+# ADD roles/bigquery.dataViewer
+resource "google_project_iam_member" "add_bigquery_data_viewer_slz_blr" {
+  project = "sdw-non-conf-3be8be-8231"
+  role    = "roles/bigquery.dataViewer"
+  member  = "group:slz-blr@google.com"
+}
