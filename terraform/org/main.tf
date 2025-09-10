2d376ba83e553cf24c9dbd7777c5d2ccdbfab4a7
neilhanda83 <neilhanda2@gmail.com>
2025-09-10 18:33:57 +0000

    [Gemini Generated] [SCC] Remediation for finding: 0246953e9e30454fbdf7ded6b983d914, Project: 228379321299, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,25 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM binding changes for project "rashi-pam-test-2"
+
+# Remove roles/owner for user:ankurdua@google.com
+resource "google_project_iam_member_remove" "remove_owner_ankurdua" {
+  project = "rashi-pam-test-2"
+  role    = "roles/owner"
+  member  = "user:ankurdua@google.com"
+}
+
+# Add roles/appmetadata.workspaceMarketplaceAppConfigurationAdmin for user:ankurdua@google.com
+resource "google_project_iam_member" "add_appmetadata_admin_ankurdua" {
+  project = "rashi-pam-test-2"
+  role    = "roles/appmetadata.workspaceMarketplaceAppConfigurationAdmin"
+  member  = "user:ankurdua@google.com"
+}
+
+# Add roles/billing.projectManager for user:ankurdua@google.com
+resource "google_project_iam_member" "add_billing_project_manager_ankurdua" {
+  project = "rashi-pam-test-2"
+  role    = "roles/billing.projectManager"
+  member  = "user:ankurdua@google.com"
+}
