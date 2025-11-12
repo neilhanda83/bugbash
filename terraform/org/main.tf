4904cc4324fa114c3948b1ba6939aa64077c6930
neilhanda83 <neilhanda2@gmail.com>
2025-11-12 18:34:25 +0000

    [Gemini Generated] [SCC] Remediation for finding: 5219a132a3004406aacf5ede21779893, Project: 27735116747, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,28 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM changes for user:aakankshathota@google.com
+resource "google_project_iam_member_remove" "remove_project_iam_admin_aakankshathota" {
+  project = data.google_project.current.project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "user:aakankshathota@google.com"
+}
+
+resource "google_project_iam_member" "add_custom_role_aakankshathota" {
+  project = data.google_project.current.project_id
+  role    = "organizations/9454078371/roles/CustomRole262"
+  member  = "user:aakankshathota@google.com"
+}
+
+# IAM changes for user:gkmr@google.com
+resource "google_project_iam_member_remove" "remove_project_iam_admin_gkmr" {
+  project = data.google_project.current.project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "user:gkmr@google.com"
+}
+
+resource "google_project_iam_member" "add_custom_role_gkmr" {
+  project = data.google_project.current.project_id
+  role    = "organizations/9454078371/roles/CustomRole262"
+  member  = "user:gkmr@google.com"
+}
