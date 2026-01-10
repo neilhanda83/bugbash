2db7ce7336a9944d39e795127331dc859dd0a7a9
neilhanda83 <neilhanda2@gmail.com>
2026-01-10 12:52:05 +0000

    [Gemini Generated] [SCC] Remediation for finding: 48fc96b6ef274504be75667858990c91, Project: 306502557873, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,32 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM Binding updates based on the task requirements for project pam-e2e-testing-sandbox (ID: 600587461297)
+
+# REMOVE roles/resourcemanager.projectIamAdmin for user:sharmarajdaksh@google.com
+resource "google_project_iam_member_remove" "remove_project_iam_admin_sharmarajdaksh" {
+  project = data.google_project.current.project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "user:sharmarajdaksh@google.com"
+}
+
+# ADD organizations/9454078371/roles/CustomRole262 for user:sharmarajdaksh@google.com
+resource "google_project_iam_member" "add_custom_role_sharmarajdaksh" {
+  project = data.google_project.current.project_id
+  role    = "organizations/9454078371/roles/CustomRole262"
+  member  = "user:sharmarajdaksh@google.com"
+}
+
+# REMOVE roles/resourcemanager.projectIamAdmin for user:divyanshas@google.com
+resource "google_project_iam_member_remove" "remove_project_iam_admin_divyanshas" {
+  project = data.google_project.current.project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "user:divyanshas@google.com"
+}
+
+# ADD organizations/9454078371/roles/CustomRole262 for user:divyanshas@google.com
+resource "google_project_iam_member" "add_custom_role_divyanshas" {
+  project = data.google_project.current.project_id
+  role    = "organizations/9454078371/roles/CustomRole262"
+  member  = "user:divyanshas@google.com"
+}
