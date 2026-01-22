3a514d81974fdc5a9ad240ab828703829538043f
neilhanda83 <neilhanda2@gmail.com>
2026-01-22 13:03:44 +0000

    [Gemini Generated] [SCC] Remediation for finding: b187c4a1a3084c9a8ba9cf9eef35df36, Project: 427145785919, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,15 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM binding for pam-dp-maf-staging project
+resource "google_project_iam_member_remove" "remove_network_admin_for_environmentgate_admin" {
+  project = "pam-dp-maf-staging"
+  role    = "roles/compute.networkAdmin"
+  member  = "serviceAccount:environmentgate-admin@pam-dp-maf-staging.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "add_regional_endpoint_viewer_for_environmentgate_admin" {
+  project = "pam-dp-maf-staging"
+  role    = "roles/networkconnectivity.regionalEndpointViewer"
+  member  = "serviceAccount:environmentgate-admin@pam-dp-maf-staging.iam.gserviceaccount.com"
+}
