f252d33c244a569b78054a1f3c33c1a15ef2217d
neilhanda83 <neilhanda2@gmail.com>
2026-04-04 07:09:35 +0000

    [Gemini Generated] [SCC] Remediation for finding: 9dd8a47891ad419081781105c6c5c60d, Project: 126373167084, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,32 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM bindings for project sdw-data-ing-3ec740-0ed8
+
+# Add roles/cloudbuild.builds.viewer for group:slz-blr@google.com
+resource "google_project_iam_member" "iam_member_add_cloudbuild_viewer_slz_blr" {
+  project = "sdw-data-ing-3ec740-0ed8"
+  role    = "roles/cloudbuild.builds.viewer"
+  member  = "group:slz-blr@google.com"
+}
+
+# Add roles/dataflow.developer for group:slz-blr@google.com
+resource "google_project_iam_member" "iam_member_add_dataflow_developer_slz_blr" {
+  project = "sdw-data-ing-3ec740-0ed8"
+  role    = "roles/dataflow.developer"
+  member  = "group:slz-blr@google.com"
+}
+
+# Remove roles/cloudbuild.builds.editor for group:slz-blr@google.com
+resource "google_project_iam_member_remove" "iam_member_remove_cloudbuild_editor_slz_blr" {
+  project = "sdw-data-ing-3ec740-0ed8"
+  role    = "roles/cloudbuild.builds.editor"
+  member  = "group:slz-blr@google.com"
+}
+
+# Remove roles/dataflow.admin for group:slz-blr@google.com
+resource "google_project_iam_member_remove" "iam_member_remove_dataflow_admin_slz_blr" {
+  project = "sdw-data-ing-3ec740-0ed8"
+  role    = "roles/dataflow.admin"
+  member  = "group:slz-blr@google.com"
+}
