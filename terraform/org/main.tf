fef6ee1e6726b4eee10c32ff621c6b4ff9d2f1ed
neilhanda83 <neilhanda2@gmail.com>
2026-06-29 15:56:39 +0000

    [Gemini Generated] [SCC] Remediation for finding: 02cc82d42ba742fca56c3cfe77bb51a4, Project: 407015958566, Category UNUSED_IAM_ROLE

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,9 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# Remove the 'roles/serviceusage.serviceUsageConsumer' role for the specified service account
+resource "google_project_iam_member_remove" "iam_member_remove_service_usage_consumer" {
+  project = data.google_project.current.project_id
+  role    = "roles/serviceusage.serviceUsageConsumer"
+  member  = "serviceAccount:overwatch-playbooks@akieras-ip.iam.gserviceaccount.com"
+}
