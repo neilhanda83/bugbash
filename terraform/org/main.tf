cd4670e5be99281fd6b5a8d654067c8bda9efb0e
neilhanda83 <neilhanda2@gmail.com>
2026-08-03 19:45:24 +0000

    [Gemini Generated] [SCC] Remediation for finding: 2e9f7298ea374c2e8d2d498228a48af6, Project: 987019150108, Category IAM_ROLE_HAS_EXCESSIVE_PERMISSIONS

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -75,3 +75,16 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# IAM Binding for REMOVE action: roles/storage.admin for user:balajiramani@google.com
+resource "google_project_iam_member_remove" "remove_storage_admin_balajiramani" {
+  project = data.google_project.current.project_id
+  role    = "roles/storage.admin"
+  member  = "user:balajiramani@google.com"
+}
+
+# IAM Binding for ADD action: roles/storage.insightsCollectorService for user:balajiramani@google.com
+resource "google_project_iam_member" "add_storage_insights_balajiramani" {
+  project = data.google_project.current.project_id
+  role    = "roles/storage.insightsCollectorService"
+  member  = "user:balajiramani@google.com"
+}
